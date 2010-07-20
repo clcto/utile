39b837835459229189e885010dd71c86e93f33f7
Carick Wienke <cdwienke@gmail.com>
2010-07-20 19:20:42 -0400

    maps event type to a string

---
+++ src/include/eventnames.h
@@ -0,0 +1,26 @@
+/*
+ * eventnames.h
+ *   maps the event type int to it's string
+ *
+ * From the Xlib Programming Manual
+ */
+
+static char* event_str[] = 
+{
+/* 00-03 */ "", "", "KeyPress", "KeyRelease",
+/* 04,05 */ "ButtonPress", "ButtonRelease",
+/* 06,07 */ "MotionNotify", "EnterNotify",
+/* 08-10 */ "LeaveNotify", "FocusIn", "FocusOut",
+/* 11,12 */ "KeymapNotify", "Expose",
+/* 13,14 */ "GraphicsExpose", "NoExpose",
+/* 15,16 */ "VisibilityNotify", "CreateNotify",
+/* 17,18 */ "DestroyNotify", "UnmapNotify",
+/* 19-21 */ "MapNotify", "MapRequest", "ReparentNotify",
+/* 22,23 */ "ConfigureNotify", "ConfigureRequest",
+/* 24,25 */ "GravityNotify", "ResizeNotify",
+/* 26,27 */ "CirculateNotify", "CirculateRequest",
+/* 28,29 */ "PropertyNotify", "SelectionClear",
+/* 30,31 */ "SelectionRequest", "SelectionNotify",
+/* 32,33 */ "ColormapNotify", "ClientMessage",
+/* 34    */ "MappingNotify"
+};
