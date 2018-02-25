b14cb0ba041cd4fb51a6dd0dcb7336d63198e797
clcto <carick.wienke@gmail.com>
2018-02-25 19:19:31 +0000

    modern c++, memory safety

---
+++ src/Command/ResizeCmd.cpp
@@ -0,0 +1,37 @@
+/*
+ * ResizeCmd.cpp
+ */
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+#include "Command.hpp"
+#include "Commands.hpp"
+#include "utile.hpp"
+
+ResizeCmd::ResizeCmd()
+{
+   usage = "";
+}
+
+void ResizeCmd::execute( const std::vector<std::string>& params )
+{
+  // if( params.size() < 2 )
+  // {
+  //    utile::log.write( LogLevel::Trace, "Horizontal Split" );
+  //    utile::split( Split::Horizontal );
+  // }
+  // else if( params[1] == "h" || params[1] == "H" )
+  // {
+  //    utile::log.write( LogLevel::Trace, "Horizontal Split" );
+  //    utile::split( Split::Horizontal );
+  // }
+  // else if( params[1] == "v" || params[1] == "v" )
+  // {
+  //    utile::log.write( LogLevel::Trace, "Vertical Split" );
+  //    utile::split( Split::Vertical );
+  // }
+  // else
+  //    utile::log.write( LogLevel::Warning, "Unrecognized split command" );
+}
